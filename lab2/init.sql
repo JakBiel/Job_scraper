bc8de5ef6d28ee2c0d37b58c31920b22aecd5467
JakBiel <jakbiel1@gmail.com>
2023-09-12 18:16:16 +0200

    Kolejny maly sukces walidacji danych - teraz usuwa nie tylko nawiasy klamrowe, ale takze dziwne biale znaki w srodku i porzadkuje je

--- lab2/init.sql
+++ lab2/init.sql
@@ -6,33 +6,33 @@
 --     main_location VARCHAR(255),
 --     other_location VARCHAR(255)
 -- );
-CREATE TABLE new_offers (
-    id SERIAL PRIMARY KEY,
-    offer_link VARCHAR(255),
-    offer_name VARCHAR(255),
-    company VARCHAR(255),
-    main_location VARCHAR(255),
-    other_location VARCHAR(255),
-    salary VARCHAR(255),
-    salary_type VARCHAR(255),
-    main_requirements_description TEXT,
-    main_offer_description TEXT,
-    your_responsibilities TEXT,
-    offer_details TEXT,
-    equipment_supplied TEXT,
-    methodology TEXT,
-    perks_in_the_office TEXT,
-    benefits TEXT,
-    company_info_Founded_in VARCHAR(255),
-    company_info_Company_size VARCHAR(255),
-    company_info_Main_location VARCHAR(255),
-    date_of_scrapping DATE,
-    when_published_relatively VARCHAR(255),
-    categories TEXT,
-    skills_maturity VARCHAR(255),
-    tags_mandatory TEXT,
-    tags_nice_to_have TEXT
-);
+-- CREATE TABLE new_offers (
+--     id SERIAL PRIMARY KEY,
+--     offer_link VARCHAR(255),
+--     offer_name VARCHAR(255),
+--     company VARCHAR(255),
+--     main_location VARCHAR(255),
+--     other_location VARCHAR(255),
+--     salary VARCHAR(255),
+--     salary_type VARCHAR(255),
+--     main_requirements_description TEXT,
+--     main_offer_description TEXT,
+--     your_responsibilities TEXT,
+--     offer_details TEXT,
+--     equipment_supplied TEXT,
+--     methodology TEXT,
+--     perks_in_the_office TEXT,
+--     benefits TEXT,
+--     company_info_Founded_in VARCHAR(255),
+--     company_info_Company_size VARCHAR(255),
+--     company_info_Main_location VARCHAR(255),
+--     date_of_scrapping DATE,
+--     when_published_relatively VARCHAR(255),
+--     categories TEXT,
+--     skills_maturity VARCHAR(255),
+--     tags_mandatory TEXT,
+--     tags_nice_to_have TEXT
+-- );
 
 -- -- Dodaj nowe kolumny z właściwymi typami danych
 -- ALTER TABLE new_offers
@@ -67,4 +67,4 @@ CREATE TABLE new_offers (
 -- ALTER TABLE new_offers
 -- RENAME COLUMN tags_nice_to_have_new TO tags_nice_to_have;
 
--- TRUNCATE new_offers RESTART IDENTITY;
+TRUNCATE new_offers RESTART IDENTITY;
